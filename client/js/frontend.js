5df9d0156ed50fee21afc75354dbdd85566e322d
Maksimillian <ismatilaevmuzaffar6@gmail.com>
2022-06-19 22:40:19 +0300

    finish

--- client/js/frontend.js
+++ client/js/frontend.js
@@ -1,20 +1,52 @@
-async function request(url, method = 'GET', data = null) {
-    try {
-      const headers = {}
-      let body
-  
-      if (data) {
-        headers['Content-Type'] = 'application/json'
-        body = JSON.stringify(data)
-      }
-  
-      const response = await fetch(url, {
-        method,
-        headers,
-        body
-      })
-      return await response.json()
-    } catch (e) {
-      console.warn('Error:', e.message)
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+
+
+Vue.component('loader', {
+  template: `
+              
+            `
+})
+
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      loading: false,
+      form: {
+        mahsulot: '',
+        son: '',
+        manzil: '',
+        telefon: ''
+      },
+      
     }
+  },
+  computed: {
+   
+  },
+  methods: {
+   
+    
   }
+})
+
+async function request(url, method = 'GET', data = null) {
+  try {
+    const headers = {}
+    let body
+
+    if (data) {
+      headers['Content-Type'] = 'application/json'
+      body = JSON.stringify(data)
+    }
+
+    const response = await fetch(url, {
+      method,
+      headers,
+      body
+    })
+    return await response.json()
+  } catch (e) {
+    console.warn('Error:', e.message)
+  }
+}
